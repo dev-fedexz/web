3b99c05d6ad7d086e94244c3b7a13a57a63a31c5
Xyzz <devfedexyz@gmail.com>
2026-01-22 16:28:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
