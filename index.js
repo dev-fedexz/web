1ffcb13313f5a99fd435ba2b10421da16bbf90b9
Xyzz <devfedexyz@gmail.com>
2026-01-22 16:24:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'lib')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'lib', 'login.html'));
+});
+
+app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'lib', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.redirect('/');
 });
 
 const PORT = process.env.PORT || 3000;
