369d0ef2298af51d4f9ee4ff624df129cf00636d
Xyzz <devfedexyz@gmail.com>
2026-01-23 10:28:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const multer = require('multer');
 const fs = require('fs');
 const app = express();
 
+
 const uploadDir = path.join(__dirname, 'lib', 'uploads');
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
+
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -26,9 +28,11 @@ const upload = multer({ storage: storage });
 app.use(express.static(path.join(__dirname, 'lib')));
 app.use('/uploads', express.static(uploadDir));
 
+
 app.post('/upload-file', upload.single('file'), (req, res) => {
   if (!req.file) return res.status(400).send('No file.');
   
+
   const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
   res.json({ url: fileUrl });
 });
