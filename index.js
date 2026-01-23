b51935019dd3ea4f2fe8620dcee695a6df14e96e
Xyzz <devfedexyz@gmail.com>
2026-01-23 10:41:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
+const os = require('os');
 const app = express();
 
+const uploadDir = path.join(os.tmpdir(), 'uploads');
 
-const uploadDir = path.join(__dirname, 'lib', 'uploads');
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    const uniqueId = Math.random().toString(36).substring(2, 12); // Genera el ID
+    const uniqueId = Math.random().toString(36).substring(2, 12);
     const finalExt = file.mimetype.startsWith('image/') ? '.jpeg' : '.mp4';
     cb(null, uniqueId + finalExt);
   }
@@ -24,19 +24,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-
 app.use(express.static(path.join(__dirname, 'lib')));
-app.use('/uploads', express.static(uploadDir));
 
+app.use('/uploads', express.static(uploadDir));
 
 app.post('/upload-file', upload.single('file'), (req, res) => {
   if (!req.file) return res.status(400).send('No file.');
-  
-
   const fileUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
   res.json({ url: fileUrl });
 });
-
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'lib', 'index.html'));
@@ -44,5 +40,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor en puerto ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
