e2315041315d231a7dad0264f1b3141edf644cc4
Xyzz <devfedexyz@gmail.com>
2026-01-25 12:32:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,48 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
+const { Redis } = require('@upstash/redis');
 const app = express();
 
-
-let storageDB = {}; 
+const redis = new Redis({
+  url: 'TU_UPSTASH_REDIS_REST_URL',
+  token: 'TU_UPSTASH_REDIS_REST_TOKEN',
+});
 
 const storage = multer.memoryStorage();
 const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 10 * 1024 * 1024 }
+    limits: { fileSize: 5 * 1024 * 1024 }
 });
 
 app.use(express.static(path.join(__dirname, 'lib')));
 
-app.get('/uploads/:filename', (req, res) => {
-    const file = storageDB[req.params.filename];
+app.get('/uploads/:filename', async (req, res) => {
+    
+    const file = await redis.get(req.params.filename);
+    
     if (file) {
+        const buffer = Buffer.from(file.buffer, 'base64');
         res.set('Content-Type', file.mime);
-        res.send(file.buffer);
+        res.send(buffer);
     } else {
-        res.status(404).send('Archivo no encontrado o expirado por inactividad');
+        res.status(404).send('Archivo no encontrado o expirado (Límite 1 semana)');
     }
 });
 
-app.post('/upload-file', upload.single('file'), (req, res) => {
+app.post('/upload-file', upload.single('file'), async (req, res) => {
     if (!req.file) return res.status(400).json({ error: 'No file' });
 
     const timestampId = Date.now();
-    let ext = path.extname(req.file.originalname) || '.bin';
-
-    if (req.file.mimetype.startsWith('image/')) ext = '.jpeg';
-    else if (req.file.mimetype.startsWith('video/')) ext = '.mp4';
-    else if (req.file.mimetype.startsWith('audio/')) ext = '.mp3';
-
+    const ext = path.extname(req.file.originalname) || '.bin';
     const fileName = `${timestampId}${ext}`;
 
-    storageDB[fileName] = {
-        buffer: req.file.buffer,
-        mime: req.file.mimetype,
-        date: Date.now()
+    const fileData = {
+        buffer: req.file.buffer.toString('base64'),
+        mime: req.file.mimetype
     };
+    
+    await redis.set(fileName, fileData, { ex: 604800 });
 
     const fileUrl = `https://${req.get('host')}/uploads/${fileName}`;
     res.json({ url: fileUrl });
@@ -52,9 +54,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'lib', 'index.html'));
 });
 
-module.exports = app; 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`✿ Servidor activo.`);
-});
+module.exports = app;
